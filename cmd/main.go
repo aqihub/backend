dd3277eba100b3193a00e43b47f874daa0c8fe2b
MadhuS-1605 <madhusgowda56@gmail.com>
2025-08-30 20:38:00 +0530

    updated dockerfile

--- cmd/main.go
+++ cmd/main.go
@@ -15,8 +15,8 @@ func main() {
 
 	router := routes.SetupRouter()
 
-	log.Println("Starting server on :8080...")
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	log.Println("Starting server on :3000...")
+	if err := http.ListenAndServe(":3000", router); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
 }
